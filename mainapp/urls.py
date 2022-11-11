3f1a035ae871974f571f519429215102028abf8b
ruvl <ru.vlad.13@gmail.com>
2022-11-11 16:27:47 +0300

    Added address, login form in <nav>, changed style.css

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
 
     path('register/', register, name='register'),
     path('login/', login_, name='login'),
+    path('profile/', user_profile, name='profile'),
+    path('logout/', logout_, name='logout'),
+
     path('search-result/', search_result, name='search_result'),
 
     path('news/', news, name='news'),
@@ -25,9 +28,7 @@ urlpatterns = [
 
     path('product/<int:pk>', get_product, name='product'),
     path('type/<int:pk>', get_products_sorted_by_type, name='type'),
-    path('collection/<int:pk>', get_types_sorted_by_collections, name='collection'),
-    path('profile/', user_profile, name='profile'),
-    path('logout/', logout_, name='logout')
+    # path('collection/<int:pk>', get_types_sorted_by_collections, name='collection'),
 ]
 
 
